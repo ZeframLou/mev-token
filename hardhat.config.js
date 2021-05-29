11bf4e7fe8fb5727730dcea2765ad685b3d0a75f
Zefram Lou <zeframlou@gmail.com>
2021-05-29 01:50:57 -0700

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,82 @@
+require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-web3");
+require("solidity-coverage");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-solhint");
+require("hardhat-spdx-license-identifier");
+require("hardhat-docgen");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-contract-sizer");
+require("@typechain/hardhat");
+
+let secret;
+
+try {
+  secret = require("./secret.json");
+} catch {
+  secret = {
+    account: "",
+    mnemonic: ""
+  };
+}
+
+module.exports = {
+  solidity: {
+    version: "0.8.3",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 1
+    }
+  },
+  paths: {
+    sources: "./contracts"
+  },
+  networks: {
+    mainnet: {
+      url:
+        "https://eth-mainnet.alchemyapi.io/v2/pvGDp1uf8J7QZ7MXpLhYs_SnMnsE0TY5",
+      chainId: 1,
+      from: secret.account,
+      accounts: {
+        mnemonic: secret.mnemonic
+      },
+      gas: "auto",
+      gasPrice: 84.0000001e9
+    },
+    hardhat: {
+      forking: {
+        url:
+          "https://eth-mainnet.alchemyapi.io/v2/pvGDp1uf8J7QZ7MXpLhYs_SnMnsE0TY5"
+      }
+    }
+  },
+  spdxLicenseIdentifier: {
+    runOnCompile: true
+  },
+  docgen: {
+    except: [],
+    clear: true,
+    runOnCompile: false
+  },
+  mocha: {
+    timeout: 60000
+  },
+  etherscan: {
+    apiKey: "SCTNNP3MJK18WV84QIX6WPGMWIS8H1J9W7"
+  },
+  gasReporter: {
+    currency: "USD",
+    coinmarketcap: "b0c64afd-6aca-4201-8779-db8dc03e9793"
+  },
+  typechain: {
+    target: "truffle-v5"
+  }
+};
